c82779e16a49eb85a52d98dfed5ac451f1cea4c7
Andreas Olsson <noname.olsson@gmail.com>
2022-06-05 22:06:19 +0200

    Add Trunk

--- app/routes/timelines.tsx
+++ app/routes/timelines.tsx
@@ -1,7 +1,7 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { PlusIcon, DotsVerticalIcon } from "@heroicons/react/outline";
+import { PlusIcon } from "@heroicons/react/outline";
 import { Page } from "~/components/page";
 import { requireUserId } from "~/session.server";
 import { getTimelineListItems } from "~/models/timeline.server";
