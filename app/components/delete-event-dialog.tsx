b36fbe7ee9b5a7975a5ca3d5f9dcd8a7c85c6bc1
Andreas Olsson <noname.olsson@gmail.com>
2022-06-10 07:42:05 +0200

    Format files

--- app/components/delete-event-dialog.tsx
+++ app/components/delete-event-dialog.tsx
@@ -61,11 +61,11 @@ export function DeleteEventDialog({
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+              <Dialog.Panel className='relative overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
                 <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
                   <div className='sm:flex sm:items-start'>
                     {icon && (
-                      <div className='mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:mr-4 sm:h-10 sm:w-10'>
+                      <div className='mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:mr-4 sm:h-10 sm:w-10'>
                         {icon}
                       </div>
                     )}
@@ -86,19 +86,19 @@ export function DeleteEventDialog({
                     </div>
                   </div>
                 </div>
-                <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'>
+                <div className='bg-gray-50 py-3 px-4 sm:flex sm:flex-row-reverse sm:px-6'>
                   {}
                   <button
                     form='delete-event'
                     type='submit'
-                    className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
+                    className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
                     onClick={handleClick}
                   >
                     Delete
                   </button>
                   <button
                     type='button'
-                    className='mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
+                    className='mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm'
                     onClick={onCloseClick}
                     ref={cancelButtonRef}
                   >
--- app/routes/timeline/$timelineId/events/$eventId.tsx
+++ app/routes/timeline/$timelineId/events/$eventId.tsx
@@ -56,7 +56,7 @@ export default function EventDetailsPage() {
           <button
             form='delete-event'
             type='submit'
-            className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
+            className='inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm'
           >
             Delete
           </button>
