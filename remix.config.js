3d8eb8628d399b8e4a2a33bdfaca723065a8efc8
Andreas Olsson <noname.olsson@gmail.com>
2022-06-19 18:41:08 +0200

    Add vercel configs

--- remix.config.js
+++ remix.config.js
@@ -6,7 +6,8 @@ module.exports = {
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  server:
+    process.env.NODE_ENV === 'development' ? undefined : '.server/index.js',
   ignoredRouteFiles: ['**/.*']
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
