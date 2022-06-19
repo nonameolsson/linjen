afca681a59d03968c107a6b6bb277e7f1b1d845f
Andreas Olsson <noname.olsson@gmail.com>
2022-06-19 23:39:47 +0200

    Add LogRocket

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,11 @@
 import { RemixBrowser } from '@remix-run/react'
+import LogRocket from 'logrocket'
+import setupLogRocketReact from 'logrocket-react'
 import { hydrate } from 'react-dom'
+
+const appId: string = (process.env.LOG_ROCKET_APP_ID = 'cvy8on/linje')
+
+LogRocket.init(appId)
+setupLogRocketReact(LogRocket)
 
 hydrate(<RemixBrowser />, document)
