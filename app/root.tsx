322ce0a225b3648a37625d760326c23577047082
Andreas Olsson <noname.olsson@gmail.com>
2022-06-20 22:23:12 +0200

    Check for authentication on routes
    Close: #42

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,7 @@ import {
 } from '@remix-run/react'
 
 import type { EnvironmentVariables } from './entry.server'
+import { getUser } from './session.server'
 
 import tailwindStylesheetUrl from './styles/tailwind.css'
 
@@ -30,14 +31,18 @@ export const meta: MetaFunction = () => ({
 
 type LoaderData = {
   ENV: EnvironmentVariables
+  user: Awaited<ReturnType<typeof getUser>>
 }
 
-export const loader: LoaderFunction = () => {
+export const loader: LoaderFunction = async ({ request }) => {
   const ENV: EnvironmentVariables = {
     LOG_ROCKET_APP_ID: process.env.LOG_ROCKET_APP_ID || ''
   }
 
-  return json({ ENV })
+  return json<LoaderData>({
+    ENV,
+    user: await getUser(request)
+  })
 }
 
 export default function App() {
