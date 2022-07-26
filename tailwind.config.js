47ee552ec9976358643fc0aa7cdba5e26fdc5413
Andreas Olsson <noname.olsson@gmail.com>
2022-07-26 21:07:24 +0000

    UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./app/**/*.{ts,tsx,jsx,js}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        rose: colors.rose
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
