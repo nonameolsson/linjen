4dad36e75716cf88394f5b9ee50e2d58252dd148
Andreas Olsson <noname.olsson@gmail.com>
2022-08-08 09:29:40 +0000

    Close #184 - Add slide over

--- app/components/fab.tsx
+++ app/components/fab.tsx
@@ -4,20 +4,26 @@ import classNames from 'classnames'
 export function Fab({
   link,
   icon,
-  offset
+  offset,
+  onClick
 }: {
-  link: string
+  link?: string
   icon: JSX.Element
   offset: boolean
+  onClick?: () => void
 }) {
   const cx = classNames(
     'btn-xl btn btn-primary btn-circle fixed right-4 shadow-2xl drop-shadow-lg hover:drop-shadow-xl active:drp-shadow-2xl',
     offset ? 'bottom-20 lg:bottom-4' : 'bottom-4'
   )
 
-  return (
+  return link ? (
     <Link to={link} className={cx}>
       {icon}
     </Link>
+  ) : (
+    <div onClick={onClick} className={cx}>
+      {icon}
+    </div>
   )
 }
