f62ed5b34e288f500bd78167ab0e8c63a0baadf4
Andreas Olsson <noname.olsson@gmail.com>
2022-09-29 18:34:18 +0200

    Delete link again

--- app/components/link-list/link-list.tsx
+++ app/components/link-list/link-list.tsx
@@ -1,57 +1,8 @@
-import { PaperClipIcon } from '@heroicons/react/solid'
 import { ActionIcon, Group, Paper, Title } from '@mantine/core'
-import { useFetcher } from '@remix-run/react'
 import { IconPlus } from '@tabler/icons'
-import cx from 'classnames'
+
 import { ExternalLinkItem } from '../external-link-item'
 import type { Link, LinkListProps } from './link-list.types'
-
-function LinkItem({ link }: { link: Link }) {
-  const { icon, id, title, url } = link
-  const fetcher = useFetcher()
-  const isDeleting = fetcher.submission?.formData.get('linkId') === link.id
-
-  const classNames = cx(
-    'flex items-center justify-between text-sm transition-opacity duration-100 hover:bg-gray-50',
-    {
-      'opacity-0': isDeleting
-    }
-  )
-
-  return (
-    <li key={title} className={classNames}>
-      <a
-        href={url}
-        target='_blank'
-        rel='noreferrer'
-        className='fontonNewClick-medium text-primary hover:text-primary-focus flex flex-1 py-3 pl-3 pr-4'
-      >
-        <div className='flex w-0 flex-1 items-center'>
-          {icon ? (
-            icon
-          ) : (
-            <PaperClipIcon
-              className='h-5 w-5 flex-shrink-0 text-gray-400'
-              aria-hidden='true'
-            />
-          )}
-          <span className='ml-2 w-0 flex-1 truncate'>{title}</span>
-        </div>
-      </a>
-      <div className='ml-4 flex-shrink-0 py-3 pl-3 pr-4'>
-        <fetcher.Form method='post' action={`/externallink/${id}`}>
-          <input type='hidden' name='linkId' value={id} />
-          <button
-            className='text-secondary hover:text-secondary-focus'
-            type='submit'
-          >
-            Delete
-          </button>
-        </fetcher.Form>
-      </div>
-    </li>
-  )
-}
 
 export function LinkList(props: LinkListProps) {
   const { items, onNewClick, title } = props
